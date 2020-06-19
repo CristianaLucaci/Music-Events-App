124077c7e23ee57914054a5fd46a315ad8d4e4bb
Cristiana <cristiana.lucaci@student.upt.ro>
2020-06-20 01:59:59 +0300

    changed the Participant class

--- src/main/java/sample/model/Participant.java
+++ src/main/java/sample/model/Participant.java
@@ -1,32 +1,62 @@
 package sample.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Participant {
-    private String participantName;
-    private String emailAddress;
-    private String username;
-    private String password;
-    private Double sold;
+   private String firstName;
+   private String lastName;
+   private String email;
+   private String phoneNumber;
+   private String username;
+   private String password;
+   private List<String> genres;
+   private Double sold;
+   private ArrayList<Event> events;
 
-    public Participant(){
+   public Participant(){
+       this.firstName="";
+       this.lastName="";
+       this.email="";
+       this.phoneNumber="";
+       this.username="";
+       this.password="";
+       this.genres=null;
+       this.sold=0.0;
+       this.events=null;
+   }
 
+   //getters and setters
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getParticipantName() {
-        return participantName;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setParticipantName(String participantName) {
-        this.participantName = participantName;
+    public String getLastName() {
+        return lastName;
     }
 
-    public String getEmailAddress() {
-        return emailAddress;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public String getUsername() {
@@ -45,12 +75,36 @@ public class Participant {
         this.password = password;
     }
 
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
     public Double getSold() {
         return sold;
     }
 
     public void setSold(Double sold) {
         this.sold = sold;
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(ArrayList<Event> events) {
+        this.events = events;
+    }
+
+    public void viewUpcomingEvents(){
+       //view event
+    }
+
+    public void soldUpdate(){
+       //update the Participant Sold
     }
 }
 
