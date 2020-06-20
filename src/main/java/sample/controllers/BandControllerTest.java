544371618740590dcbdbd1535d85a9e34acb964f
Florin <florinxhgds@gmail.com>
2020-06-20 13:07:22 +0300

    #2 Added Band Test

---
+++ src/main/java/sample/controllers/BandControllerTest.java
@@ -0,0 +1,23 @@
+package sample.controllers;
+
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.assertEquals;
+class BandControllerTest {
+
+    @Test
+    void seeEventOffer() {
+    }
+
+    @Test
+    void seeEventDetails() {
+    }
+}
