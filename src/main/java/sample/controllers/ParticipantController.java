0d553ebe022da3b3880bcc41e8b87b051c35f111
Cristiana <cristiana.lucaci@student.upt.ro>
2020-06-20 19:09:18 +0300

    MEA-31 Changed the display of the Participant page

--- src/main/java/sample/controllers/ParticipantController.java
+++ src/main/java/sample/controllers/ParticipantController.java
@@ -1,4 +1,33 @@
 package sample.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import sample.model.User;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ParticipantController {
+
+    @FXML
+    private Text text;
+    @FXML
+    private Text text1;
+
+    private User currentUser;
+    @FXML
+    public void initialize(){
+        currentUser = LoginController.getCurrentUser();
+        text.setText("User: " + currentUser.getText2());
+    }
+
 }
