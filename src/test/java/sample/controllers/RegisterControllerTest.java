daee2e7cec6a17a5e0fa09fd04ea8b42e795f26d
Florin <florinxhgds@gmail.com>
2020-06-20 15:29:25 +0300

    #4

--- src/test/java/sample/controllers/RegisterControllerTest.java
+++ src/test/java/sample/controllers/RegisterControllerTest.java
@@ -1,10 +1,22 @@
 package sample.controllers;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import sample.services.FileSystemService;
+import sample.services.UserService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RegisterControllerTest {
+class RegisterControllerTest extends ApplicationTest {
+
+    public static final String TEST_USER = "testUser";
+    public static final String TEST_PASSWORD = "testPassword";
+    private RegisterController controller;
+
+    
 
     @Test
     void registerButtonClicked() {
