cc996e989c828a9021b408c213632e442d231078
Florin <florinxhgds@gmail.com>
2020-06-23 10:33:38 +0300

    #6

--- src/test/java/sample/controllers/RegisterControllerTest.java
+++ src/test/java/sample/controllers/RegisterControllerTest.java
@@ -12,6 +12,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterControllerTest extends ApplicationTest {
 
+    public static final String TEXT1 = "Name";
+    public static final String TEXT2 = "ASDF";
+    public static final String EMAIL = "email";
+    public static final String PHONE = "23542354235";
     public static final String TEST_USER = "testUser";
     public static final String TEST_PASSWORD = "testPassword";
     private RegisterController controller;
