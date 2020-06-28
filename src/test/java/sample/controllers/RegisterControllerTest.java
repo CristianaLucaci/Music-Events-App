6b008ae21dfbfa3de50b846f98da11e54638d2e5
Florin <florinxhgds@gmail.com>
2020-06-28 12:01:17 +0300

    MEA-42

--- src/test/java/sample/controllers/RegisterControllerTest.java
+++ src/test/java/sample/controllers/RegisterControllerTest.java
@@ -1,5 +1,11 @@
 package sample.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -7,6 +13,12 @@ import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit.ApplicationTest;
 import sample.services.FileSystemService;
 import sample.services.UserService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,12 +30,46 @@ class RegisterControllerTest extends ApplicationTest {
     public static final String PHONE = "23542354235";
     public static final String TEST_USER = "testUser";
     public static final String TEST_PASSWORD = "testPassword";
+    public static List<String> GENRES = new ArrayList<>();
     private RegisterController controller;
 
-    
+    @BeforeClass
+    public static void setupClass() throws Exception {
+        FileSystemService.APPLICATION_FOLDER = ".test-registration-example";
+        FileSystemService.initApplicationHomeDirIfNeeded();
+        UserService.loadUsersFromFile();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomePath().toFile());
+        UserService.loadUsersFromFile();
+
+        controller = new RegisterController();
+        controller.usernameField = new TextField();
+        controller.passwordField = new PasswordField();
+        controller.userBox = new CheckBox();
+        controller.eventManagerBox = new CheckBox();
+        controller.bandBox = new CheckBox();
+        controller.registrationMessage = new Text();
+        controller.textField1 = new TextField();
+        controller.textField2 = new TextField();
+        controller.emailField = new TextField();
+        controller.phoneNumberField = new TextField();
+
+        controller.textField1.setText(TEXT1);
+        controller.textField2.setText(TEXT2);
+        controller.emailField.setText(EMAIL);
+        controller.phoneNumberField.setText(PHONE);
+        controller.usernameField.setText(TEST_USER);
+        controller.passwordField.setText(TEST_PASSWORD);
+        controller.genres.add("Rock");
+    }
 
     @Test
-    void registerButtonClicked() {
+    public void registerButtonClickedTest() throws IOException {
+        controller.registerButtonClicked2();
+        assertEquals(1, UserService.getUsers().size());
     }
 
     @Test
